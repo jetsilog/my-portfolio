f37f84a04bad819c96e4ff06233db42e47e3f807
jetsilog <jhonearvintangonan@gmail.com>
2023-11-28 21:20:16 +0800

    bug fixes

--- src/components/Portfolio.jsx
+++ src/components/Portfolio.jsx
@@ -1,23 +1,23 @@
-import React from "react";
-import damsashs from "../assets/portfolio/damsashs.png";
+import React from 'react';
+import damsashs from '../assets/portfolio/damsashs.png';
 
 const Portfolio = () => {
   return (
     <div
-      name="portfolio"
-      className="bg-white dark:bg-gradient-to-b from-black to-gray-800 text-gray-500 dark:text-white md:h-screen"
+      name='portfolio'
+      className='bg-white dark:bg-gradient-to-b from-black to-gray-800 text-gray-500 dark:text-white h-screen'
     >
-      <div className="max-w-screen-lg p-4 mx-auto flex flex-col justify-center w-full h-full">
-        <div className="pb-8">
-          <p className="text-4xl font-bold inline">Project</p>
-          <p className="py-6">My very first big project hehe</p>
+      <div className='max-w-screen-lg p-4 mx-auto flex flex-col justify-center w-full h-full'>
+        <div className='pb-8'>
+          <p className='tex-black text-4xl font-bold inline'>Project</p>
+          <p className='py-6'>My very first big project hehe</p>
         </div>
-        <div className="flex flex-col mx-auto items-center justify-center md:px-12 sm:px-12">
-          <div className="shadow-md shadow-gray-600 rounded-lg mb-10">
+        <div className='flex flex-col mx-auto items-center justify-center md:px-12 sm:px-12'>
+          <div className='mb-10 shadow-md shadow-gray-600 rounded-lg '>
             <img
               src={damsashs}
-              alt=""
-              className="rounded-md duration-500 hover:scale-105"
+              alt=''
+              className='rounded-md duration-500 hover:scale-105'
             />
             {/*
             <div className="flex items-center justify-center">
--- src/components/Skills.jsx
+++ src/components/Skills.jsx
@@ -1,87 +1,87 @@
-import React from "react";
-import html from "../assets/html.png";
-import css from "../assets/css.png";
-import js from "../assets/javascript.png";
-import php from "../assets/php.png";
-import tailwind from "../assets/tailwind.png";
-import bootstrap from "../assets/bootstrap.png";
-import react from "../assets/react.png";
-import node from "../assets/node.png";
+import React from 'react';
+import html from '../assets/html.png';
+import css from '../assets/css.png';
+import js from '../assets/javascript.png';
+import php from '../assets/php.png';
+import tailwind from '../assets/tailwind.png';
+import bootstrap from '../assets/bootstrap.png';
+import react from '../assets/react.png';
+import node from '../assets/node.png';
 
 const Skills = () => {
   const techs = [
     {
       id: 1,
       src: html,
-      title: "HTML",
-      style: "shadow-orange-500",
+      title: 'HTML',
+      style: 'shadow-orange-500',
     },
     {
       id: 2,
       src: css,
-      title: "CSS",
-      style: "shadow-blue-500",
+      title: 'CSS',
+      style: 'shadow-blue-500',
     },
     {
       id: 3,
       src: js,
-      title: "JavaScript",
-      style: "shadow-yellow-500",
+      title: 'JavaScript',
+      style: 'shadow-yellow-500',
     },
     {
       id: 4,
       src: php,
-      title: "PHP",
-      style: "shadow-purple-500",
+      title: 'PHP',
+      style: 'shadow-purple-500',
     },
     {
       id: 5,
       src: tailwind,
-      title: "Tailwind",
-      style: "shadow-sky-400",
+      title: 'Tailwind',
+      style: 'shadow-sky-400',
     },
     {
       id: 6,
       src: bootstrap,
-      title: "Bootstrap",
-      style: "shadow-purple-800",
+      title: 'Bootstrap',
+      style: 'shadow-purple-800',
     },
     {
       id: 7,
       src: react,
-      title: "React",
-      style: "shadow-cyan-800",
+      title: 'React',
+      style: 'shadow-cyan-800',
     },
     {
       id: 8,
       src: node,
-      title: "Node",
-      style: "shadow-green-800",
+      title: 'Node',
+      style: 'shadow-green-800',
     },
   ];
 
   return (
     <div
-      name="skills"
-      className="bg-white dark:bg-gradient-to-b from-gray-800 to to-black w-full h-screen mt-20 mb-20"
+      name='skills'
+      className='dark:bg-gradient-to-b from-gray-800 to to-black w-full h-screen'
     >
-      <div className="max-w-screen-lg mx-auto p-4 flex flex-col justify-center w-full h-full text-white">
+      <div className='max-w-screen-lg mx-auto p-4 flex flex-col justify-center w-full h-full text-white'>
         <div>
-          <p className="text-4xl font-bold p-2 inline text-gray-500 dark:text-white">
+          <p className='text-4xl font-bold p-2 inline text-gray-500 dark:text-white'>
             Skills
           </p>
-          <p className="py-6 text-gray-500 dark:text-white">
+          <p className='py-6 text-gray-500 dark:text-white'>
             These are the technologies I've worked with
           </p>
         </div>
-        <div className="w-full grid grid-cols-2 sm:grid-cols-3 gap-8 text-center py-8 px-12 sm:px-0">
+        <div className='w-full grid grid-cols-2 sm:grid-cols-3 gap-8 text-center py-8 px-12 sm:px-0'>
           {techs.map(({ id, src, title, style }) => (
             <div
               key={id}
               className={`shadow-md hover:scale-105 duration-500 py-2 rounded-lg ${style}`}
             >
-              <img src={src} alt="" className="w-20 mx-auto" />
-              <p className="mt-4 text-gray-500 dark:text-white">{title}</p>
+              <img src={src} alt='' className='w-20 mx-auto' />
+              <p className='mt-4 text-gray-500 dark:text-white'>{title}</p>
             </div>
           ))}
         </div>
